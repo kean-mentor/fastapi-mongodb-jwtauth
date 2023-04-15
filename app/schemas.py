69e805849967dfb6480c095a33e9fd5a59d3ca91
Andras Keszte <kesztea@gmail.com>
2023-04-15 13:50:16 +0200

    Create schemas and serializers

---
+++ app/schemas.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+from pydantic import BaseModel, EmailStr, constr
+
+
+class UserBaseSchema(BaseModel):
+    name: str
+    email: str
+    photo: str
+    role: str | None = None
+    created_at: datetime | None = None
+    updated_at: datetime | None = None
+
+    class Config:
+        orm_mode = True
+
+
+class CreateUserSchema(UserBaseSchema):
+    password: constr(min_length=8)
+    passwordConfirm: str
+    verified: bool = False
+
+
+class UserResponseSchema(UserBaseSchema):
+    id: str
+
+
+class UserResponse(BaseModel):
+    status: str
+    user: UserResponseSchema
+
+
+class LoginUserSchema(BaseModel):
+    email: EmailStr
+    password: constr(min_length=8)
